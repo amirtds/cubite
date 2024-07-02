c5b6fd14b1b4c573f17e7f4eab41aacba71e9d19
Amir Tadrisi <amirtds@gmail.com>
2024-07-02 15:44:26 -0500

    feat: user avatar and bell in navbar

--- app/components/SiteNavbar.tsx
+++ app/components/SiteNavbar.tsx
@@ -3,7 +3,8 @@
 import React from "react";
 import { Image } from "@/app/components/Image";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { BellIcon } from "@heroicons/react/24/outline";
 
 interface Site {
   name: string;
@@ -23,7 +24,22 @@ interface Props {
 
 const SiteNavbar = ({ site, headerLinks }: Props) => {
   const { status, data: session } = useSession();
-
+  const renderAvatar = () => {
+    if (session?.user?.image) {
+      return (
+        <img alt="Avatar" src={session.user.image} className="rounded-full" />
+      );
+    } else if (session?.user?.name) {
+      const initial = session.user.name.charAt(0).toUpperCase();
+      return (
+        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-500 text-white">
+          {initial}
+        </div>
+      );
+    } else {
+      return <div className="w-10 h-10 rounded-full bg-gray-300"></div>;
+    }
+  };
   return (
     <div className="bg-base-200">
       <div className="navbar mx-auto max-w-7xl p-6 lg:px-8">
@@ -85,6 +101,37 @@ const SiteNavbar = ({ site, headerLinks }: Props) => {
               </a>
             ) : null;
           })}
+          {session && (
+            <>
+              <button className="btn btn-ghost btn-circle mx-2">
+                <div className="indicator">
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  <span className="badge badge-xs badge-primary indicator-item"></span>
+                </div>
+              </button>
+              <div className="dropdown dropdown-end">
+                <div tabIndex={0} role="button" className="">
+                  {renderAvatar()}
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                >
+                  <li>
+                    <Link href={"/profile"} className="justify-between">
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/settings"}>Settings</Link>
+                  </li>
+                  <li>
+                    <button onClick={() => signOut()}>Logout</button>
+                  </li>
+                </ul>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
