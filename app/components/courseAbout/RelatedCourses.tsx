55c7c6114ba676f0936f7a6a409adf39a353871c
Amir Tadrisi <amirtds@gmail.com>
2024-07-16 16:32:29 -0500

    fix: show related course component only if there is related courses

--- app/components/courseAbout/RelatedCourses.tsx
+++ app/components/courseAbout/RelatedCourses.tsx
@@ -5,14 +5,16 @@ import CourseCard from "../CourseCard";
 
 const RelatedCourses = ({ courses, site }) => {
   return (
-    <div className="m-12">
-      <p className="text-3xl font-semibold mb-8">Related Courses</p>
-      <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-8">
-        {courses.map((course) => (
-          <CourseCard key={course.id} course={course} site={site} />
-        ))}
+    courses.length > 0 && (
+      <div className="m-12">
+        <p className="text-3xl font-semibold mb-8">Related Courses</p>
+        <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-8">
+          {courses.map((course) => (
+            <CourseCard key={course.id} course={course} site={site} />
+          ))}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
