0a68c9a08f1d1667a4355b80c9f4e1d90350f66f
Amir Tadrisi <amirtds@gmail.com>
2024-07-16 16:03:44 -0500

    fix: remove empty custom reg field on site creation

--- app/components/RegistrationFields.tsx
+++ app/components/RegistrationFields.tsx
@@ -2,11 +2,7 @@ import { PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 import React, { useState, useEffect } from "react";
 
 const RegistrationFields = ({ onFieldChange, title, existingFields }) => {
-  const [fields, setFields] = useState(
-    existingFields
-      ? existingFields
-      : [{ text: "", type: "text", required: false }]
-  );
+  const [fields, setFields] = useState(existingFields ? existingFields : []);
 
   useEffect(() => {
     onFieldChange(fields);
