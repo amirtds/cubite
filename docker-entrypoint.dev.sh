fe7801b74c653a9bf955495d97e36765b7d60b65
Amir Tadrisi <amirtds@gmail.com>
2024-11-28 17:20:54 -0500

    chore: add favicon to site and run prisma studio on dev

--- docker-entrypoint.dev.sh
+++ docker-entrypoint.dev.sh
@@ -10,6 +10,10 @@ done
 echo "Running database migrations..."
 npx prisma migrate deploy
 
+# Start Prisma Studio in the background
+echo "Starting Prisma Studio..."
+npx prisma studio --port 5555 --hostname 0.0.0.0 &
+
 # Start the development server
 echo "Starting in development mode..."
 npm run dev
---
+++ scripts.sh
@@ -0,0 +1,4 @@
+docker-compose -f docker-compose.dev.yml down
+docker-compose -f docker-compose.dev.yml up -d --build
+docker-compose -f docker-compose.dev.yml exec web npx prisma migrate dev --name add_externalImageUrl_for_courses
+docker compose exec web npx prisma migrate deploy
